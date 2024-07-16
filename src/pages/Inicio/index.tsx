81fcffb4956f3925666a3b6e2a01a2f4918d355b
Guilherme Barbosa <barbosa.guisilvaa@gmail.com>
2024-07-16 16:20:52 -0300

    implementando rota "Home"

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,14 +1,27 @@
-import Cardapio from "pages/Cardapio/Itens/itens.json";
+import cardapio from 'data/cardapio.json';
+import styles from './inicio.module.scss';
 
-export default function Inicio (){
-    return (
-        <section>
-            <h3>
-                Recomendações da cozinha
-            </h3>
-            <div>
+export default function Inicio() {
+  let pratosRecomendado = [...cardapio];
+  pratosRecomendado = pratosRecomendado.sort(() => 0.5 - Math.random()).splice(0, 3);
 
+  return (
+    <section>
+      <h3 className={styles.titulo}>
+        Recomendações da cozinha
+      </h3>
+      <div className={styles.recomendados}>
+        {pratosRecomendado.map(prato => (
+          <div key={prato.id} className={styles.recomendado}>
+            <div className={styles.recomendado__imagem}>
+              <img src={prato.photo} alt={prato.title} />
             </div>
-        </section>
-    );
+            <button className={styles.recomendado__botao}>
+              Ver mais
+            </button>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
