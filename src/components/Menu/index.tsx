d1b123cdbae68d4ed13ed7b9ae41a21723c2ee07
Guilherme Barbosa <barbosa.guisilvaa@gmail.com>
2024-07-17 16:20:03 -0300

    reaproveitando elementos entre as pages

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,9 @@
+import { ReactComponent as Logo } from 'assets/logo.svg';
+import styles from './Menu.module.scss';
+export default function Menu() {
+  return (
+    <nav className={styles.menu}>
+      <Logo />
+    </nav>
+  );
+}
