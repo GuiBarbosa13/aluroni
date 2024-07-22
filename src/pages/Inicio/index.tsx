de2f9877ee5294cab484d4d0e645cbe4bb20e7ea
Guilherme Barbosa <barbosa.guisilvaa@gmail.com>
2024-07-22 16:39:19 -0300

    finalizando página de sobre

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,6 +1,7 @@
 import cardapio from 'data/cardapio.json';
 import styles from './inicio.module.scss';
 import stylesTema from 'styles/Tema.module.scss';
+import nossaCasa from 'assets/nossa_casa.png';
 
 export default function Inicio() {
   let pratosRecomendado = [...cardapio];
@@ -22,6 +23,11 @@ export default function Inicio() {
             </button>
           </div>
         ))}
+      </div>
+      <h3 className={stylesTema.titulo}>Nossa casa</h3>
+      <div className={styles.nossaCasa}>
+        <img src={nossaCasa} alt='Casa do Aluroni'/>
+        <div className={styles.nossaCasa__endereco}>Rua vergueiro, 3185<br/><br/>Vila Mariana - SP</div>
       </div>
     </section>
   );
