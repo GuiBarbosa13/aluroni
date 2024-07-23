d806b1e3b5a644f86f0ed58e6f999c7dbbdd169d
Guilherme Barbosa <barbosa.guisilvaa@gmail.com>
2024-07-23 15:34:31 -0300

    criando botão de voltar na pagina de not found

--- src/routes.js
+++ src/routes.js
@@ -8,17 +8,20 @@ import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 export default function AppRouter() {
   return (
-    <Router>
-      <Menu/>
-      <Routes>
-        <Route path='/' element={<PaginaPadrao />}>
-          <Route index element={<Inicio />}/>
-          <Route path='cardapio' element={<Cardapio />} />
-          <Route path='sobre' element={<Sobre />} />
-        </Route>
-        <Route path='*' element={<NotFound/>}/>
-      </Routes>
-      <Footer/>
-    </Router>
+    <main className='container'>
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path='cardapio' element={<Cardapio />} />
+            <Route path='sobre' element={<Sobre />} />
+          </Route>
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </main>
+
   );
 }
